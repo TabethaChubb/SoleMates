687aa053183a0a2cf3bc9f04d822f70aa73e47ac
Gregory Mensah <gregorymensah@wifi-roaming-128-4-90-96.wifi.udel.edu>
2023-04-25 19:32:34 -0400

    added a few sneakers

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import puma from "../src/images/3.png";
 import { Sneaker } from "./interfaces/sneaker";
 import { WishList } from "./userWishList";
 import logo from "../src/images/soleMatesLogo.jpg";
+import R1 from "../src/images/R1.png";
+import s1 from "../src/images/s1.jpg";
 
 function App(): JSX.Element {
     const [currList, setWishList] = useState<Sneaker[]>([]);
@@ -75,10 +77,24 @@ function App(): JSX.Element {
                     <div id="adidas">
                         <h1>ADIDAS</h1>
                     </div>
+                    <div className="drop">
+                        <button className="sneaker1">
+                            <a href="addi1">
+                                <img src={R1}></img>
+                            </a>
+                            <div className="ad1">R1</div>
+                        </button>
+                        <button className="addi2">
+                            <a href="addi2">
+                                <img src={s1}></img>
+                            </a>
+                        </button>
+                    </div>
                 </footer>
                 <footer className="NIKE">
                     <div id="nike"></div>
                     <h1>NIKE</h1>
+                    <a href="Air Force 1">Air force 1</a>
                 </footer>
                 <footer className="PUMA">
                     <div id="puma"></div>
