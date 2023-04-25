616c6d0a6394f19c97b5fd734c5c9ad9178367f1
Tommy <tommyhaskell3@gmail.com>
2023-04-24 23:32:43 -0400

    Adding in Sneaker interface -> object properties

---
+++ src/interfaces/sneaker.ts
@@ -0,0 +1,18 @@
+/** SneakerType influences when a sneaker should show up on the main list since we have the brands in different sections */
+export type SneakerBrand = "Nike" | "Puma" | "Adidas";
+
+/** A representation of a Question in a quizzing application */
+export interface Sneaker {
+    /** The human-friendly model name of the sneaker */
+    model: string;
+    /** The list of colors that are available for a shoe */
+    colors: string[];
+    /** The brand of Sneaker; influences when a sneaker should show up on the main list since we have the brands in different sections */
+    brand: SneakerBrand;
+    /** The List of available sizes for this sneaker */
+    size: number[];
+    /** How much money this sneaker costs the user */
+    price: number;
+    /** Whether or not this sneaker is out of stock */
+    outOfStock: boolean;
+}
