7ecf52ac35007ca29b7fcb0e60a0be08a55c504e
joey.blair <joeblair@udel.edu>
2023-05-02 16:57:57 -0400

    made the NavBar a component

--- src/components/role.tsx
+++ src/components/role.tsx
@@ -1,2 +1,4 @@
+import { useState } from "react";
 export {};
-/** export type role = "Customer" | "Employee" | "Owner"; **/
+export type role = "Customer" | "Employee" | "Owner";
+export const [role, setRole] = useState<role>();
