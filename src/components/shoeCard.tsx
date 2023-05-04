d6ca4414c3e909f979a32e6b0e7ef89ec6da1337
Tommy <tommyhaskell3@gmail.com>
2023-05-04 16:21:12 -0400

    shoeCard -> ShoeCard

--- src/components/shoeCard.tsx
+++ src/components/shoeCard.tsx
@@ -5,11 +5,21 @@ interface shoeCardProp {
     sneaker: Sneaker;
 }
 
-export function shoeCard({ sneaker }: shoeCardProp): JSX.Element {
+export function ShoeCard({ sneaker }: shoeCardProp): JSX.Element {
+    const handleDragStart = (
+        event: React.DragEvent<HTMLDivElement>,
+        sneaker: Sneaker
+    ) => {
+        event.dataTransfer.setData("application/json", JSON.stringify(sneaker));
+    };
     return (
         <div>
             <div id="sneakerpage"></div>
-            <div className="productcard">
+            <div
+                className="productcard"
+                draggable="true"
+                onDragStart={(event) => handleDragStart(event, sneaker)}
+            >
                 <img src={sneaker.image}></img>
                 <h1>Sneaker Name: {sneaker.model}</h1>
                 <h2>Sneaker Brand: {sneaker.brand}</h2>
