e068756d1676cd1984745b6d833d1483709143e5
Greg <Godly@udel.edu>
2023-05-16 14:12:00 -0400

    added last shoes

--- src/nike.tsx
+++ src/nike.tsx
@@ -167,6 +167,17 @@ const inventory: Sneaker[] = [
         price: 250,
         outOfStock: false,
         image: two
+    },
+    {
+        model: "Reto GTS",
+        colors: ["white", "black", "Phantom", "Red", "Green", "yellow"],
+        selectedColor: "",
+        brand: "Nike",
+        size: [7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12],
+        selectedSize: NaN,
+        price: 200,
+        outOfStock: false,
+        image: two
     }
 ];
 
